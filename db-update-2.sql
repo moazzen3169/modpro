eccef0d121433a913f11fd77e1ffd3ca1d13e6c9
hadi3169 <moazzenhadi46@gmail.com>
2025-10-08 19:43:07 +0330

    first day im using this system

--- db-update-2.sql
+++
@@ -1,6 +0,0 @@
--- Indexes to optimize supplier-based reporting
-ALTER TABLE Purchases
-    ADD INDEX idx_purchases_supplier_date (supplier_id, purchase_date);
-
-ALTER TABLE Purchase_Returns
-    ADD INDEX idx_purchase_returns_supplier_date (supplier_id, return_date);
